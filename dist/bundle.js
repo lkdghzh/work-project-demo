3feeb138ef88be5c2b9bbcfec9b29d33a82084c3
李可 <like@likedeMacBook-Pro.local>
2024-12-28 23:40:00 +0800

    build:测试线上打包产物,非压缩

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,44 +1,52 @@
-(() => {
-  var __webpack_modules__ = {
-    "./src/a.js": (module, __unused_webpack_exports, __webpack_require__) => {
-      eval(
-        "var b=__webpack_require__(\"./src/b.js\")\nconsole.log('a',b)\nmodule.exports='a'"
-      );
-    },
-    "./src/b.js": (module) => {
-      eval(
-        "console.log('b')\nmodule.exports='b'"
-      );
-    },
-    "./src/index.js": (
-      __unused_webpack_module,
-      __unused_webpack_exports,
-      __webpack_require__
-    ) => {
-      eval(
-        "var a=__webpack_require__(\"./src/a.js\")\nconsole.log('index',a)"
-      );
-    },
-  };
-  var __webpack_module_cache__ = {};
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-  function __webpack_require__(moduleId) {
-    var cachedModule = __webpack_module_cache__[moduleId];
-    if (cachedModule !== undefined) {
-      return cachedModule.exports;
-    }
-    var module = (__webpack_module_cache__[moduleId] = {
-      // no module.id needed
-      // no module.loaded needed
-      exports: {},
-    });
+/***/ 967:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-    // 执行模块
-    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+var b=__webpack_require__(170)
+console.log('a',b)
+module.exports='a'
 
-    // 每个模块返回
-    return module.exports;
-  }
+/***/ }),
 
-  var __webpack_exports__ = __webpack_require__("./src/index.js");
-})();
+/***/ 170:
+/***/ ((module) => {
+
+console.log('b')
+module.exports='b'
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var a=__webpack_require__(967)
+console.log('index',a)
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-var path=require('path')
-module.exports={
-  mode:'development',
-   entry:'./src/index.js',
-   output:{
-    filename:'bundle.js',
-    path:path.resolve(__dirname,'dist')
-   }
-}
+var path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimize: false
+  },
+};
